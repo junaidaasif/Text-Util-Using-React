8e08d7c21c5af078e9cfe1bbfaa63379d20a2fac
Md juanid <68678685+junaidaasif@users.noreply.github.com>
2022-02-09 15:45:32 +0530

    Text util

---
+++ precache-manifest.41f37d5d1f42fb5b1f994ef06fc1e91a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a88a1747d12914465e5aec5bd8cce4c1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e89ad63cd39335668828",
+    "url": "/static/css/main.e43634d0.chunk.css"
+  },
+  {
+    "revision": "4c326c67b006f9751338",
+    "url": "/static/js/2.6484bd36.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "e89ad63cd39335668828",
+    "url": "/static/js/main.f93bed0f.chunk.js"
+  },
+  {
+    "revision": "bb1f08fe37d3c65590eb",
+    "url": "/static/js/runtime~main.e015284a.js"
+  }
+]);
